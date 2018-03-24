e151dbac1454393083a6915431f5d278948e5db1
bharathgane <37720546+bharathgane415@users.noreply.github.com>
2018-03-24 06:20:45 +0530

    assignment 1 and 2

---
+++ assignment_1.py
@@ -0,0 +1,15 @@
+# Question 1
+print("Hey!")
+print("The code is %&#!*")
+print("Please call 320288328 to verify.")
+# Question 2
+print("I'd like a Martini please. Shaken, not stirred.")
+
+# Question 3
+print "Hello World!"
+
+# Question 4
+print 'Mourinho says,"Manchester united are used to losing in the UCL."'
+#or
+print "Mourinho says,\"Manchester united are used to losing in the UCL.\""
+
